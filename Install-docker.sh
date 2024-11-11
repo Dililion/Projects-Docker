a89370c2ba6caeb7edd1afc3d831e23ec3ae1ce5
Dililion <87888437+Dililion@users.noreply.github.com>
2024-11-11 16:50:59 +0300

    Update Install-docker.sh

--- Install-docker.sh
+++ Install-docker.sh
@@ -6,21 +6,20 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-# Устанавливаем Docker
-echo "Установка Docker..."
-sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io
-sudo systemctl enable docker
-sudo systemctl start docker
+# Проверка наличия Docker
+if ! command -v docker &> /dev/null; then
+  echo "Установка Docker..."
+  wget -O - https://get.docker.com | bash
+  echo "Docker установлен."
+fi
 
-# Устанавливаем Docker Compose
-echo "Установка Docker Compose..."
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.10.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+# Проверка наличия Docker Compose
+if ! command -v docker-compose &> /dev/null; then
+  echo "Установка Docker Compose..."
+  sudo curl -L "https://github.com/docker/compose/releases/download/v2.10.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  echo "Docker Compose установлен."
+fi
 
 # Проверка установки
 echo "Проверка установки Docker и Docker Compose..."
